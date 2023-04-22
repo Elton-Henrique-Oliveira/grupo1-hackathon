fd1df9160298507eded272634480b612c3ae09b4
ELTONH.OLIVEIRA <terezafe.oli@gmail.com>
2023-04-22 09:02:30 -0300

    ...

--- src/main/kotlin/singe/internationalization/called/infraestructure/repository/database/CalledDataBase.kt
+++ src/main/kotlin/singe/internationalization/called/infraestructure/repository/database/CalledDataBase.kt
@@ -1,6 +1,11 @@
 package singe.internationalization.called.infraestructure.repository.database
 
-import org.jetbrains.exposed.sql.Table
+import br.com.lince.singe.core.shared.utils.Utils
+import br.com.lince.singe.core.shared.webservice.BasicFilter
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.lessEq
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.like
 import org.jetbrains.exposed.sql.javatime.CurrentDateTime
 import org.jetbrains.exposed.sql.javatime.datetime
 import singe.internationalization.called.domain.entities.DescriptionCalled
@@ -18,4 +23,28 @@ object CalledDataBase : Table("called") {
     init {
         PrimaryKey(uuid)
     }
+}
+
+fun Query.withCalledFilters(filter: List<BasicFilter>?): Query {
+    if (filter == null) {
+        return this
+    }
+    val filters = filter.filter { it.name.isNotBlank() }.map {
+        when (it.name) {
+            "uuid" -> Op.build { CalledDataBase.uuid eq Utils.uuidOrEmpty(it.value) }
+            "identifier" -> Op.build { CalledDataBase.identifier eq it.value.toLong()}
+            "userName" -> Op.build { CalledDataBase.userName.lowerCase() like "%" + it.value.lowercase() + "%" }
+            "modifiedAtDate" -> Op.build {
+                 (CalledDataBase.modifiedAt lessEq Utils.dateConverterMax(
+                    it.value
+                ))
+            }
+
+            else -> throw Exception("invalid column name: ${it.name}")
+        }
+    }
+
+    filters.forEach { this.andWhere { it } }
+
+    return this
 }
